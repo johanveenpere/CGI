5fe0875b3ff520c74150944c483e160a42b14919
Johan Veenpere <johan.veenpere@gmail.com>
2025-03-24 12:49:28 +0200

    init

---
+++ src/main/java/org/example/cgi/CgiApplication.java
@@ -0,0 +1,13 @@
+package org.example.cgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgiApplication.class, args);
+    }
+
+}
