4b885b1f55191b42184faf48037aca70e14316cd
Johan Veenpere <johan.veenpere@gmail.com>
2025-03-29 12:39:03 +0200

    WIP adding conditional querying

--- src/main/java/org/example/cgi/Model/FlightsRepository.java
+++ src/main/java/org/example/cgi/Model/FlightsRepository.java
@@ -1,6 +1,19 @@
 package org.example.cgi.Model;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface FlightsRepository extends JpaRepository<Flight, Long> {
+public interface FlightsRepository extends JpaRepository<Flight, Long> , JpaSpecificationExecutor<Flight> {
+    static Specification<Flight> priceGreaterThanOrEqual(float minPrice) {
+        return ((root, query, criteriaBuilder) ->
+                criteriaBuilder.greaterThanOrEqualTo(root.get(Flight_.price), minPrice));
+
+    }
+
+    static Specification<Flight> priceLessThanOrEqual(float maxPrice) {
+        return ((root, query, criteriaBuilder) ->
+                criteriaBuilder.lessThanOrEqualTo(root.get(Flight_.price), maxPrice));
+    }
+
 }
--- src/main/java/org/example/cgi/WebAPI/FlightAPIController.java
+++ src/main/java/org/example/cgi/WebAPI/FlightAPIController.java
@@ -2,12 +2,14 @@ package org.example.cgi.WebAPI;
 
 import org.example.cgi.Model.Flight;
 import org.example.cgi.Model.FlightsRepository;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static org.example.cgi.Model.FlightsRepository.priceGreaterThanOrEqual;
 
 @RestController
 public class FlightAPIController {
@@ -19,11 +21,25 @@ public class FlightAPIController {
     }
 
     @GetMapping("/flights")
-    public List<Flight> flights(@RequestParam(value = "minprice", required = false) Integer minprice,
-                                @RequestParam(value = "maxprice", required = false) Integer maxprice,
+    public List<Flight> flights(@RequestParam(value = "minprice", required = false) Float minprice,
+                                @RequestParam(value = "maxprice", required = false) Float maxprice,
                                 @RequestParam(value = "location", required = false) String location,
                                 @RequestParam(value = "date", required = false) String date) {
-        return repository.findAll();
+        Specification<Flight> spec = Specification.where(null);
+        if (minprice != null) {
+            spec = spec.and(priceGreaterThanOrEqual(minprice));
+        }
+        if (maxprice != null) {
+            spec = spec.and(priceLessThan(maxprice));
+        }
+        if (!location.isEmpty()) {
+            spec = spec.and(destination(location));
+        }
+        if (!date.isEmpty()) {
+            spec = spec.and(arrivalTime(date));
+        }
+        return repository.findAll(spec);
     }
+
 
 }
