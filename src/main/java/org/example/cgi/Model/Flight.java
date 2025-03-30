d47787e5405ca32a06c9f49204fb2d7d02cb2c1f
Johan Veenpere <johan.veenpere@gmail.com>
2025-03-30 11:58:28 +0300

    Refactored the Flight entity

--- src/main/java/org/example/cgi/Model/Flight.java
+++ src/main/java/org/example/cgi/Model/Flight.java
@@ -1,8 +1,6 @@
 package org.example.cgi.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 
 import java.sql.Date;
 import java.sql.Time;
@@ -11,10 +9,11 @@ import java.util.List;
 @Entity
 public class Flight {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     String destination;
-    Date arrivalTime;
-    Time flightLength;
+    Date arrivaltime;
+    Time flightlength;
     float price;
     @OneToMany
     List<Seat> flightSeats;
