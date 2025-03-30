7ac273b4d21ec9c1bdaa7d928327edfa776ed74f
Johan Veenpere <johan.veenpere@gmail.com>
2025-03-30 12:03:07 +0300

    Added query methods for Flights repository

--- src/main/java/org/example/cgi/Model/FlightsRepository.java
+++ src/main/java/org/example/cgi/Model/FlightsRepository.java
@@ -4,16 +4,27 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.sql.Date;
+
 public interface FlightsRepository extends JpaRepository<Flight, Long> , JpaSpecificationExecutor<Flight> {
     static Specification<Flight> priceGreaterThanOrEqual(float minPrice) {
         return ((root, query, criteriaBuilder) ->
-                criteriaBuilder.greaterThanOrEqualTo(root.get(Flight_.price), minPrice));
-
+                criteriaBuilder.greaterThanOrEqualTo(root.get("Flight").get("price"), minPrice));
     }
 
     static Specification<Flight> priceLessThanOrEqual(float maxPrice) {
         return ((root, query, criteriaBuilder) ->
-                criteriaBuilder.lessThanOrEqualTo(root.get(Flight_.price), maxPrice));
+                criteriaBuilder.lessThanOrEqualTo(root.get("Flight").get("price"), maxPrice));
+    }
+
+    static Specification<Flight> destination(String destination) {
+        return ((root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("Flight").get("price"), destination));
+    }
+
+    static Specification<Flight> arrivalTime(Date arrivalTime) {
+        return ((root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("Flight").get("price"), arrivalTime));
     }
 
 }
